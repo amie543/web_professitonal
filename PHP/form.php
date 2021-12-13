e534c67a0b9fcf9eafe53d200b305b3ae957f522
arkarMannAung <mr.arkarmannaung@gmail.com>
2021-12-13 20:57:25 +0630

    hello

--- PHP/form.php
+++ PHP/form.php
@@ -4,13 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>HTML Method Testing</title>
+    <style>
+        .whitespace{
+            padding: 30px 0px;
+        }
+        .container{
+            width: 80%;
+            margin: auto;
+        }
+        .form-group{
+            padding: 20px 0px;
+        }
+        .form-group label{
+            height: 30px;
+            line-height: 30px;
+            width: 30%;
+            display: inline-block;
+        }
+        .form-group input{
+            height: 30px;
+            line-height: 30px;
+            display: inline-block;
+            width: 65%;
+            background: rgb(235, 231, 231);
+            border: none;
+            border-radius: 15px;
+            padding-left: 40px;
+        }
+        .form-control{
+            align-self: center;
+        }
+    </style>
 </head>
 <body>
-    <form action="index.php">
-        <input type="text" name="name" id="">Name<br><br>
-        <input type="text" name="age" id="">Age<br><br>
-        <button type="submit">Submit</button>
-    </form>
+    <div class="whitespace"></div>
+    <div class="container">
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <label for="name">Name: </label>
+                <input type="text" name="name" id="name">
+            </div>
+            <div class="form-group">
+                <label for="age">Age: </label>
+                <input type="text" name="age" id="age">
+            </div>
+            <div class="form-control">
+                <button class="btn btn-submit" type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
--- PHP/index.php
+++ PHP/index.php
@@ -1,4 +1,15 @@
 <?php
-print_r($_GET);
-
-?>
+    $request = $_SERVER['REQUEST_METHOD'];
+    echo $request .'<br>';
+    if($request=='GET'){
+        print_r($_GET);
+        echo '<br><br>Method is GET';
+    }elseif($request=='POST'){
+        echo 'Invalid Method<br> Your Request POST Method Not Allow';
+    }elseif($request=='PUT'){
+        echo 'Invalid Method<br> Your Request PUT Method Not Allow';
+    }elseif($request=='DELETE'){
+        echo 'Invalid Method<br> Your Request DELETE Method Not Allow';
+    }else {
+        echo 'Invalid Request';
+    }
